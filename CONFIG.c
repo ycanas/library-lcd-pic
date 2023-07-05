a22917e96d9493dffbc5101adb16045e386d2df5
Yair Cañas <58963999+ycanas@users.noreply.github.com>
2023-07-05 11:51:23 -0500

    config files

---
+++ CONFIG.c
@@ -0,0 +1,11 @@
+#include <xc.h>
+#include "CONFIG.h"
+
+#pragma config FOSC  = XT
+#pragma config WDTE  = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP   = OFF
+#pragma config CPD   = OFF
+#pragma config WRT   = OFF
+#pragma config CP    = OFF
---
+++ CONFIG.h
@@ -0,0 +1,12 @@
+/* 
+ * File: config.h
+ * Author: ycanas
+ * Revision history: 03/07/2023
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#include <xc.h>
+#define _XTAL_FREQ 4000000
+#endif
