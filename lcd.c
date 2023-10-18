e5535f6624ec3bb6c87c9a365871dfa7cff7c1b8
Yair Cañas <58963999+ycanas@users.noreply.github.com>
2023-10-18 00:34:00 -0500

    Update and rename LCD.c to lcd.c

---
+++ lcd.c
@@ -0,0 +1,146 @@
+
+/*
+ * File: lcd.c
+ * Author: ycanas
+ * Revision history: 17/10/2023
+ */
+
+
+#include "config.h"
+#include "lcd.h"
+
+void cmd(unsigned char cmd) {
+
+    /******************* NIBLE ALTO *******************/
+    
+    PORTLCD = 0;
+    
+    D4 = (cmd >> 4) & 1;
+    D5 = (cmd >> 5) & 1;
+    D6 = (cmd >> 6) & 1;
+    D7 = (cmd >> 7) & 1;
+    
+    E = 0;
+    
+    __delay_us(100);
+    
+    E = 1;
+    
+    __delay_us(100);
+    
+    E = 0;
+    
+    __delay_us(100);
+    
+    /******************* NIBLE BAJO *******************/
+    
+    PORTLCD = 0;
+    
+    D4 = 1 & (cmd >> 0);
+    D5 = 1 & (cmd >> 1);
+    D6 = 1 & (cmd >> 2);
+    D7 = 1 & (cmd >> 3);
+    
+    E = 0;
+    
+    __delay_us(100);
+    
+    E = 1;
+    
+    __delay_us(100);
+    
+    E = 0;
+    
+    __delay_us(100);
+    PORTLCD = 0;
+}
+
+
+void str(unsigned char cmd) {
+    
+    /******************* NIBLE ALTO *******************/
+    
+    PORTLCD = 0;
+    
+    D4 = (cmd >> 4) & 1;
+    D5 = (cmd >> 5) & 1;
+    D6 = (cmd >> 6) & 1;
+    D7 = (cmd >> 7) & 1;
+    
+    RS = 1;
+    E  = 0;
+    
+    __delay_us(100);
+    
+    E = 1;
+    
+    __delay_us(100);
+    
+    E  = 0;
+    RS = 0;
+    
+    __delay_us(100);
+    
+    /******************* NIBLE BAJO *******************/
+    
+    PORTLCD = 0;
+    
+    D4 = 1 & (cmd >> 0);
+    D5 = 1 & (cmd >> 1);
+    D6 = 1 & (cmd >> 2);
+    D7 = 1 & (cmd >> 3);
+    
+    RS = 1;
+    E  = 0;
+    
+    __delay_us(100);
+    
+    E  = 1;
+    
+    __delay_us(100);
+    
+    E  = 0;
+    RS = 0;
+    
+    __delay_us(100);
+    PORTLCD = 0;
+}
+
+
+void LCD_Init() {
+    TRISLCD = 0;
+    PORTLCD = 0;
+    
+    __delay_ms(20);
+    
+    cmd(0x03);
+    __delay_ms(10);
+    
+    cmd(0x03);
+    __delay_us(150);
+    
+    for (int i = 0; i < 6; i++) {
+        cmd(CMDs[i]);
+        __delay_us(100);
+    }
+}
+
+
+void LCD_SetCursor(unsigned char y, unsigned char x) {
+    cmd(POSy[y] + x);
+    __delay_us(100);
+}   
+
+
+void LCD_Print(char *string) {
+    while (*string) {
+        str(*string);
+        string++;
+    }
+}
+
+
+void LCD_Clear() {
+    cmd(0x01);
+    __delay_ms(1);
+}
