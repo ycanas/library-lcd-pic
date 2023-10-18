f877ce071b63733c50a3e8a5e3394cc3fc8d12cd
Yair Cañas <58963999+ycanas@users.noreply.github.com>
2023-10-18 00:34:32 -0500

    Update and rename CONFIG.h to config.h

--- config.h
+++ config.h
@@ -1,12 +1,16 @@
+
 /* 
  * File: config.h
  * Author: ycanas
- * Revision history: 03/07/2023
+ * Revision history: 17/10/2023
  */
 
+ 
 #ifndef CONFIG_H
 #define	CONFIG_H
 
 #include <xc.h>
+
 #define _XTAL_FREQ 4000000
+
 #endif
