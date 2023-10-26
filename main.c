ddbc6a8df6722318638478fc08b3e025fc14d230
Yair Cañas <58963999+ycanas@users.noreply.github.com>
2023-10-25 22:23:07 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,28 +11,28 @@
 #include "lcd.h"
 
 void main(void) {
-    LCD_Init();
-    LCD_SetCursor(0, 0);
-    LCD_Print("Iniciando...");
+    lcd_init();
+    lcd_setcursor(0, 0);
+    lcd_print("Iniciando...");
     __delay_ms(1000);
     
     while (1) {
-        LCD_Clear();
+        lcd_clear();
         
-        LCD_SetCursor(0, 0);
-        LCD_Print("Hola Mundo");
+        lcd_setcursor(0, 0);
+        lcd_print("Hola Mundo");
         __delay_ms(1);
         
-        LCD_SetCursor(1, 0);
-        LCD_Print("LCD");
+        lcd_setcursor(1, 0);
+        lcd_print("LCD");
         __delay_ms(1);
         
-        LCD_SetCursor(2, 0);
-        LCD_Print("16x2");
+        lcd_setcursor(2, 0);
+        lcd_print("16x2");
         __delay_ms(1);
         
-        LCD_SetCursor(3, 0);
-        LCD_Print("& 20x4");
+        lcd_setcursor(3, 0);
+        lcd_print("& 20x4");
         __delay_ms(1000);
     }
 }
